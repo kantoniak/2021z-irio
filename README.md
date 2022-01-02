8e0cfb7788f7c67ad57badca6f9e6e1d7d98bd95
Krzysztof Antoniak <kantoniak@kantoniak.com>
2022-01-02 03:30:35 +0100

    Infra: Set up WorkInProgress deployment

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # 2021z-irio
+
+## Building with Google Deployment Manager
+
+1. Create a [Source Repository](https://source.cloud.google.com/repo/new) (named `alerting-platform`) in the target project.
+2. Add remote and push this code to the repository.
+3. Deploy with Google Deployment Manager:
+    ```
+    gcloud deployment-manager deployments update prod --config infra/deploy-prod.yaml
+    ```
+4. Enable public access to `WorkInProgress`:
+    ```
+    gcloud functions add-iam-policy-binding WorkInProgress \
+      --region="europe-central2"
+      --member="allUsers" \
+      --role="roles/cloudfunctions.invoker"
+    ```
+5. You may want to connect to database to create table and insert sample data from `sample-data.sql`:
+    ```
+    gcloud sql connect services-instance -d services-db -u <username>
+    ```
--- sample-data.sql
+++ sample-data.sql
@@ -1,2 +1,16 @@
+-- Functions will create this table on first call
+CREATE TABLE IF NOT EXISTS "services" (
+    "id" INT NOT NULL,
+    "name" VARCHAR(256) NOT NULL,
+    "url" TEXT NOT NULL,
+    "primary_admin_email" VARCHAR(254) NOT NULL,
+    "secondary_admin_email" VARCHAR(254) NOT NULL,
+    "last_time_responsive" TIMESTAMP NULL,
+    "being_worked_on" BOOLEAN NOT NULL DEFAULT FALSE,
+    "primary_admin_key" UUID NULL,
+    "secondary_admin_key" UUID NULL,
+    CONSTRAINT "services_pk" PRIMARY KEY (id)
+);
+
 INSERT INTO "services" ("id", "name", "url", "primary_admin_email", "secondary_admin_email", "last_time_responsive", "being_worked_on", "primary_admin_key", "secondary_admin_key") VALUES
     (1, 'example', 'https://example.com/', 'admin1@example.com', 'admin2@example.com', NULL, FALSE, '6d18c09e-f47b-41ba-a293-fd6c978c1e66', 'a16c337f-4bc5-4ab5-84b7-0e7f5cf52850');

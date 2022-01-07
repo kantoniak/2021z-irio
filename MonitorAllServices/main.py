b1385646ad64635ee5922c9c6b0eda9632472d8d
dawid <dawidbiadun12@gmail.com>
2022-01-07 16:42:01 +0100

    Add MonitorAllServices function (with config)

---
+++ MonitorAllServices/main.py
@@ -0,0 +1,231 @@
+import base64
+import datetime
+import logging
+import os
+import requests
+import sqlalchemy
+import time
+import uuid
+from google.cloud import logging as cloudlogging
+from sqlalchemy.sql import text
+from mailjet_rest import Client
+from google.cloud import tasks_v2
+import json
+
+
+
+
+cloudlogging.Client().setup_logging()
+logger = logging.getLogger()
+
+DB_CONN_NAME = os.getenv('DB_CONN_NAME')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_DATABASE = os.getenv('DB_DATABASE')
+REGION = os.getenv('REGION')
+PROJECT = os.getenv('PROJECT_NAME')
+
+ALLOWED_RESPONSE_TIME = os.getenv('ALLOWED_RESPONSE_TIME')
+REQUEST_TIMEOUT_SEC = os.getenv('REQUEST_TIMEOUT_SEC')
+ALERTING_WINDOW_SEC = os.getenv('ALERTING_WINDOW_SEC')
+
+if DB_CONN_NAME is None:
+    raise RuntimeError('Missing environment variable: DB_CONN_NAME')
+if DB_USERNAME is None:
+    raise RuntimeError('Missing environment variable: DB_USERNAME')
+if DB_PASSWORD is None:
+    raise RuntimeError('Missing environment variable: DB_PASSWORD')
+if DB_DATABASE is None:
+    raise RuntimeError('Missing environment variable: DB_DATABASE')
+if REGION is None:
+    raise RuntimeError('Missing environment variable: REGION')
+if PROJECT is None:
+    raise RuntimeError('Missing environment variable: PROJECT')
+
+api_key = '13bde1f003f14dfe019284c8839ec9fa' # TODO : fix this
+api_secret = '4e64b4ac3672eec18b0fbdc4d79a1817' # TODO : fix this
+MY_MAIL = "automatedmailer@protonmail.com"
+MAILJET = Client(auth=(api_key, api_secret), version='v3.1')
+def send_mail(recipient, service_name):
+    data = {
+        "Messages": [
+            {
+                "From": {
+                    "Email": MY_MAIL,
+                    "Name": "Alerting platform automatic mailer",
+                },
+                "To": [{"Email": recipient, "Name": recipient}],
+                "Subject": "Warning, service is down",
+                "TextPart": "Warning, service {} is down!".format(service_name),
+                "HTMLPart": "",
+                "CustomID": "",
+            }
+        ]
+    }
+
+    result = MAILJET.send.create(data=data)
+    return result.json()
+
+def schedule_task(secondary_admin_email, service_name, delay):
+    # https://cloud.google.com/tasks/docs/samples/cloud-tasks-taskqueues-new-task
+    # https://cloud.google.com/tasks/docs/creating-http-target-tasks
+    client = tasks_v2.CloudTasksClient()
+    parent = client.queue_path(PROJECT, REGION, QUEUE_NAME)
+
+    task = {
+        'http_request': {
+            "http_method": tasks_v2.HttpMethod.POST,
+            "url": "https://" + REGION + "-" + PROJECT + ".cloudfunctions.net/" + FUNC_TO_CALL,
+            'headers': {
+                "Content-Type": "application/json"
+            },
+            "oidc_token": {
+                "service_account_email": PROJECT + "@appspot.gserviceaccount.com"
+            },
+            "body": json.dumps({"secondary_admin_email" : secondary_admin_email, "service_name" : service_name}).encode("utf-8"),
+        },
+        'schedule_time': datetime.datetime.now() + datetime.timedelta(seconds=delay) 
+    }
+    return client.create_task(request={"parent": parent, "task": task})
+
+
+def init_pool(conn_name, username, password, database):
+    pool = sqlalchemy.create_engine(
+        sqlalchemy.engine.url.URL.create(
+            drivername="postgresql+pg8000",
+            username=username,
+            password=password,
+            database=database,
+            query={
+                "unix_sock": "/cloudsql/{}/.s.PGSQL.5432".format(conn_name)
+            }
+        ),
+        pool_size=5,
+        max_overflow=2,
+        pool_timeout=30,
+        pool_recycle=1800
+    )
+    pool.dialect.description_encoding = None
+    return pool
+
+
+def initialize_db(conn):
+    conn.execute("""
+CREATE TABLE IF NOT EXISTS "services" (
+    "id" INT NOT NULL,
+    "name" VARCHAR(256) NOT NULL,
+    "url" TEXT NOT NULL,
+    "primary_admin_email" VARCHAR(254) NOT NULL,
+    "secondary_admin_email" VARCHAR(254) NOT NULL,
+    "last_time_responsive" TIMESTAMP NULL,
+    "being_worked_on" BOOLEAN NOT NULL DEFAULT FALSE,
+    "primary_admin_key" UUID NULL,
+    CONSTRAINT "services_pk" PRIMARY KEY (id)
+);""")
+
+# FIXME: Move to MonitorAllServices once created
+def initialize_task_queue():
+    # https://cloud.google.com/tasks/docs/samples/cloud-tasks-create-queue#cloud_tasks_create_queue-python
+    client = tasks_v2.CloudTasksClient()
+
+    # Construct the fully qualified location path.
+    parent = f"projects/{PROJECT}/locations/{REGION}"
+
+    all_queues = client.list_queues(request={"parent": parent})
+    for queue in all_queues:
+        if queue.name.split("/")[-1] == QUEUE_NAME:
+            return None
+
+    # Construct the create queue request.
+    queue = {"name": client.queue_path(PROJECT, REGION, QUEUE_NAME)}
+    # Use the client to create the queue.
+    response = client.create_queue(request={"parent": parent, "queue": queue})
+
+    return response
+
+
+def handle_service_up(service, conn):
+    conn.execute(
+        text("UPDATE services SET last_time_responsive = :datetime WHERE id = :id"),
+        {
+            'id': service['id'],
+            'datetime': datetime.datetime.now()
+        }
+    )
+
+
+def handle_service_down(service, conn):
+    time_now = datetime.datetime.now()
+
+    since_last_check = (time_now - service['last_time_responsive']).total_seconds()
+    if since_last_check > ALERTING_WINDOW_SEC:
+        new_incident = True  # FIXME: How do we know current downtime had/had not been detected?
+
+        if new_incident:
+            service_name = service['name']
+            logger.info(f'Service "{service_name}" down.')
+
+            primary_key = str(uuid.uuid4())
+            conn.execute(
+                text("UPDATE services SET being_worked_on = FALSE, primary_admin_key = :primary_key WHERE id = :id"),
+                {
+                    'id': service['id'],
+                    'primary_key': primary_key
+                }
+            )
+            send_mail(service['primary_admin_email'], service_name)
+            schedule_task(service['secondary_admin_email'], service_name, ALLOWED_RESPONSE_TIME)
+
+def entrypoint(event, _):
+    initialize_task_queue()
+
+    # Decode incoming ID
+    if 'data' in event:
+        service_id = int(base64.b64decode(event['data']).decode('utf-8'))
+    else:
+        raise RuntimeError('Service ID missing.')
+
+    # Init connection
+    db = init_pool(
+        DB_CONN_NAME,
+        DB_USERNAME,
+        DB_PASSWORD,
+        DB_DATABASE
+    )
+
+    with db.connect() as conn:
+        initialize_db(conn)
+
+        # Fetch service details
+        service = conn.execute(
+            text("SELECT id, name, url, last_time_responsive, primary_admin_email, secondary_admin_email FROM services WHERE id = :id"),
+            { 'id': service_id }
+        ).fetchone()
+
+        if not service:
+            raise RuntimeError(f'No service with ID {service_id}.')
+
+        url = service['url']
+        name = service['name']
+
+        if not service['last_time_responsive']:
+            logger.warn(f'Service "{name}" has not been up yet.')
+            return
+
+        # Check if service up
+        try:
+            response = requests.get(
+                url,
+                allow_redirects=True,
+                timeout=(REQUEST_TIMEOUT_SEC, REQUEST_TIMEOUT_SEC),
+            )
+
+            if response.status_code < 200 or 300 <= response.status_code:
+                logger.info(f'Service "{name}": request to {url} returned HTTP {response.status_code}.')
+                handle_service_down(service, conn)
+            else:
+                handle_service_up(service, conn)
+
+        except requests.exceptions.Timeout:
+            logger.info(f'Service "{name}": request to {url} timed out after {REQUEST_TIMEOUT_SEC} s.')
+            handle_service_down(service, conn)
